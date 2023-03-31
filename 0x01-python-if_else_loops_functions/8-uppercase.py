888bba7b76f89607a6c6fde192f5b592afc48fc9
gedithecoder <gedionderejev@gmail.com>
2023-03-31 04:04:54 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return True
-    else:
-        return False
+def uppercase(str):
+    str = chr(ord(str) - 32)
+    for i in str:
+        print(str, end='')
+print("\n")
