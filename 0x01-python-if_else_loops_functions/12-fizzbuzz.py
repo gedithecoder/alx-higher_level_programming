fb382235e40010ea883c233afed3fc9abcf93dbf
gedithecoder <gedionderejev@gmail.com>
2023-04-04 18:28:25 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+for i in range(1, 101):
+    if (i % 3 = 0) and (i % 5 = 0):
+        print("{}".format(FizzBuzz), end=" ")
+    elif (i % 3 == 0):
+        print("{}".format(Fizz), end=" ")
+    elif (i % 5 == 0):
+        print("{}".format(Buzz), end=" ")
+    else:
+        print("{}".format(i), end=" ")
+    
