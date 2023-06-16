8f5762ae73f3ddd1a52eab14b9e0ffa82a221baa
gedithecoder <gedionderejev@gmail.com>
2023-06-16 14:07:04 -0700

    Write a script that searches the second biggest integer in the list of arguments.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArg = process.argv.slice(2);
+if (myArg.length === 0) {
+  console.log('0');
+} else if (myArg.length === 1) {
+  console.log('0');
+} else { 
+  const argv = myArg.sort(function(a, b){return b - a});
+  console.log(argv[1]);
+}
