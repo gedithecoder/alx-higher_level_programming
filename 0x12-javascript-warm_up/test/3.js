8a5ce9ee7fc71d82a352dd8b6b669cc716aa07de
gedithecoder <117764141+gedithecoder@users.noreply.github.com>
2023-11-14 00:15:10 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/test/3.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const myArg = process.argv.slice(2);
-if (myArg[0] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(myArg[0]);
-}
--- 0x12-javascript-warm_up/test/5.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const myArg = process.argv.slice(2);
-if (isNaN(myArg[0])) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${myArg[0]}`);
-}
