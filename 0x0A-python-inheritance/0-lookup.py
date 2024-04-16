257a437dd83862185740ed6984bea4ca27d4941a
gedithecoder <gedionderejev@gmail.com>
2024-04-15 19:35:25 -0700

    Hot fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""  returns the list of available attributes
-and methods of an object
+"""
+===========================
+Module with class MyList
+===========================
 """
 
 
-def lookup(obj):
-    """returns the list of available attributes"""
-    return dir(obj)
+class MyList(list):
+    """Class with method print_sorted"""
+    pass
+
+    def print_sorted(self):
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
