234e4aa2b08f4c7ab30da870ebf5ac70e12213c1
gedithecoder <gedionderejev.com>
2024-08-09 03:37:33 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-for i in range(0, 89):
-    if (i / 10) < (i % 10):
-        print("{:02}".format(i), end=", ")
-print("89")
+
+for x in range(0, 10):
+
+    for y in range(x + 1, 10):
+
+        if x == 8 and y == 9:
+
+            print('89')
+
+        else:
+
+            print('{}{}, '.format(x, y), end='')
