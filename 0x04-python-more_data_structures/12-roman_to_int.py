e0db0d1124da1de7d71f2fe79ef05d535f7d7beb
gedithecoder <gedionderejev.com>
2024-08-10 04:31:12 -0700

    Hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,12 @@
 
 
 def roman_to_int(roman_string):
-    """
-    Convert a roman numeral to an integer
+    """ Convert a roman numeral to an integer.
+
     You can assume the number will be between 1 to 3999.
     If the roman_string is not a string or None, return 0
     """
+
     if type(roman_string) != str or roman_string is None:
         return 0
     roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
